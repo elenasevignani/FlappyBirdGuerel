009a9f510fe7e22f013bfdf18eb42916b8b15294
Teto Gürel <tateguerel@gmail.com>
2018-10-24 14:13:31 +0200

    Codeless Programming Steps Complete

---
+++ Assets/MyGame/Scripts/TapController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))] 
+public class TapController : MonoBehaviour {
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+		
+	}
